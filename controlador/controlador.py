0aa7f19ccf7161b7f52754259986e52a47b14d59
Alejandro A. Buezo <aabuezo@gmail.com>
2021-11-13 03:06:36 -0300

    ok el ida y vuelta, falta la BD y limpiar un poco el codigo

--- controlador/controlador.py
+++ controlador/controlador.py
@@ -1,20 +1,24 @@
 import socket
 import sys
 import threading
+from modelo.modelo import especialidades, turnos
 
 
 class Server:
-    def __init__(self, host='localhost', port=3000, cn=10):
+    def __init__(self, host='localhost', port=4000, cn=10):
+        self.nombre = 'Catalina  '
         self._lst_clientes = []
+        self.turno = ''
+        self.especialidad = ''
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.bind( (host, port))
         self._sock.listen(cn)
-        self._serve = threading.Thread(target=self.serve)
+        self._serve = threading.Thread(target=self.atender)
         self._serve.daemon = True
         self._serve.start()
-        self.console()
+        self.consola()
 
-    def console(self):
+    def consola(self):
         while True:
             command = input('-> ')
             if command == 'salir':
@@ -25,49 +29,77 @@ class Server:
                         c.close()
                 sys.exit()
 
-    def serve(self):
+    def atender(self):
         print('Esperando conexiones!')
+        print('Ingrese "salir" para finalizar')
         while True:
             cliente, addr = self._sock.accept()
             self._lst_clientes.append(cliente)
             print(f'cliente conectado desde: {addr}')
             print(cliente)
             # un thread para escuchar al cliente
-            cc = threading.Thread(target=self.listen, args=(cliente, ))
+            cc = threading.Thread(target=self.escuchar, args=(cliente,))
             cc.daemon = True
             cc.start()
 
-    def listen(self, client):
+    def escuchar(self, client):
         estado = 0
         while True:
             packet = client.recv(1024)
             if packet:
                 data = packet.decode('utf-8')
-                respuesta = self.responder(client, estado, data)
-                estado += 1
-
-    def send(self, client, msg):
-        nombre = 'Catalina  '
-        data = nombre + msg
-        packet = data.encode('utf-8')
-        client.send(packet)
+                estado = self.responder(client, estado, data)
+                print(f'estado: {estado}')
+                if estado == 3:
+                    client.close()
+                    print('Se cerro la conexion')
+                    self._lst_clientes.remove(client)
+                    print('Se quito al cliente de la lista')
+                    return
 
     def responder(self, cliente, estado, data):
         respuesta = ''
-        opcion = 0
-        turno = 0
+        opcion = ''
         nombre = data[:10]
-        mensaje = data[10:]
+        mensaje = data[10]
         print(str(nombre).rstrip() + ': ' + str(mensaje))
         if estado == 0:
-            respuesta = 'Bienvenido al sistema de turnos!\nMi nombre es Catalina, por favor, elija la opcion que desea:\n' + '1. Oftalmologia\n' + '2. Pediatría\n' + '3. Obstetricia\n' + '4. Ginecología'
-        elif estado == 1:   # respondio la opcion
-            opcion = mensaje[0]
-            respuesta = 'Elija su turno:\n1. Lunes\n2. Martes\n3. Miércoles\n4. Jueves\n5. Viernes\n'
-        elif estado == 2:   # responde el turno
-            turno = mensaje[0]
-            respuesta = f'Le confirmo su turno de {opcion} para el dia {turno}\n'
-        self.send(cliente, respuesta)
+            respuesta = self.menu_especialidades()
+            estado += 1
+        elif estado == 1:   # ya respondio la opcion
+            opcion = mensaje
+            if opcion in especialidades.keys():
+                self.especialidad = especialidades[opcion]
+                respuesta = self.menu_turnos()
+                estado += 1
+            else:
+                respuesta = 'Ingrese su opcion nuevamente'
+        elif estado == 2:   # ya respondio el turno
+            turno = mensaje
+            if turno in turnos.keys():
+                self.turno = turnos[turno]
+                respuesta = f'Le confirmamos su turno de {self.especialidad} para el dia {self.turno}!\nAdiós!'
+                estado += 1
+            else:
+                respuesta = 'Ingrese su opcion nuevamente'
+        data = self.nombre + respuesta
+        packet = data.encode('utf-8')
+        cliente.send(packet)
+        return estado
+
+    def menu_especialidades(self):
+        menu = 'Bienvenido al sistema de turnos!\n'
+        menu += 'Mi nombre es Catalina, por favor, elija la opcion que desea:\n' \
+                + f'1. {especialidades["1"]}\n' \
+                + f'2. {especialidades["2"]}\n' \
+                + f'3. {especialidades["3"]}\n' \
+                + f'4. {especialidades["4"]}'
+        return menu
+
+    def menu_turnos(self):
+        menu = 'Elija su turno:\n'
+        menu += '1. Lunes\n2. Martes\n3. Miércoles\n4. Jueves\n5. Viernes'
+        return menu
 
 
 if __name__ == '__main__':
---
+++ modelo/modelo.py
@@ -0,0 +1,18 @@
+
+
+especialidades = {
+    '1': 'Obstetricia',
+    '2': 'Oftalmologia',
+    '3': 'Pediatría',
+    '4': 'Clínica Médica'
+}
+
+turnos = {
+    '1': 'Lunes',
+    '2': 'Martes',
+    '3': 'Miércoles',
+    '4': 'Jueves',
+    '5': 'Viernes'
+}
+
+
